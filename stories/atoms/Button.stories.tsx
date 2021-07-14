d4f3393e393f640fd6e33b3a96285805864fbc71
Andressa Novaes <andressa.menezes@eemovel.com>
2021-07-14 15:21:32 -0300

    feat: implement icons on the button

--- stories/atoms/Button.stories.tsx
+++ stories/atoms/Button.stories.tsx
@@ -1,7 +1,38 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { Story } from '@storybook/react';
+import { action } from '@storybook/addon-actions'
 
+import { Colors } from '../../src/presentation/styles/colors.enum';
 import Button from '../../src/presentation/components/atoms/button/Button';
 // import Button from '@/presentation/components/atoms/button/Button';
+import { faTrash, faCheck, faComments } from '@fortawesome/free-solid-svg-icons'
 
-storiesOf('Button', module).add('Default', () => <Button>Default</Button>);
+export default {
+  title: 'Atom/Button',
+  component: Button,
+  argTypes: {
+    style: { borderColor: { control: 'color' }},
+    onClick: {action: 'clicked'},
+    iconLeft: {
+      control: {
+        type: "select",
+        options: {
+            Without: null,
+            TrashIcon: faTrash,
+            CheckIcon: faCheck,
+            Comment: faComments,
+        },
+      }
+    }
+  },
+};
+
+const Template: Story = (args) => <Button {...args} />;
+
+export const basic = Template.bind({});
+basic.args = { 
+  label: 'Button', 
+  disabled: false,
+  style: {borderColor: Colors.GOLD},
+  onClick: action('clicked'),
+};
